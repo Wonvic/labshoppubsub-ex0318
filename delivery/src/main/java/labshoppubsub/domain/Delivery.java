40f96bcb193797f5b6fb73b36c131adc8f5ab0ed
seungwon Lee <next1113@naver.com>
2025-03-18 02:12:46 +0000

    add logic

--- delivery/src/main/java/labshoppubsub/domain/Delivery.java
+++ delivery/src/main/java/labshoppubsub/domain/Delivery.java
@@ -36,12 +36,12 @@ public class Delivery {
     public static void addDelivery(OrderPlaced orderPlaced) {
         //implement business logic here:
 
-        /** Example 1:  new item 
         Delivery delivery = new Delivery();
+        delivery.setCustomerId(orderPlaced.getCustomerId());
+        delivery.setAddress(orderPlaced.getAddress());
+        // delivery.setStatus(status:"READY");
         repository().save(delivery);
-
-        */
-
+    
         /** Example 2:  finding and process
         
 
